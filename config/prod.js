b199bba5acdf837b04ae93aa2d1981154498d30a
王诗尧 <shiyaow@ous-MacBook-puro.local>
2020-11-01 20:53:21 -0800

    added billing and client side up

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,12 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
+
+//https://hidden-thicket-35281.herokuapp.com/auth/google	
 
 //484329394444-sgc2a9jacf0ek8q1prptp40kg9mgafs0.apps.googleusercontent.com
 //LhGmoZYcOi_1dBM8NroKbeDt
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require('body-parser');
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -14,6 +15,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express(); 
 
+
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,//cookies lasting 30 days
@@ -25,6 +28,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doens't recognize the route 
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT);
