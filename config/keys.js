e4f715972d11dddeab2031bb045d3c277dbb47e5
王诗尧 <shiyaow@ous-MacBook-puro.local>
2020-11-02 20:47:13 -0800

    ingore .ds_store

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
 // keys.js - figure out what set of credentials to return
 
-//emaily-prod
-//mongodb+srv://myuser:9b3jxMgQNhqn76Tt@cluster0.gfsoy.mongodb.net/test?retryWrites=true&w=majority
 if (process.env.NODE_ENV === 'production'){
     //we are in production - return the prod set of keys
     module.exports = require('./prod');
